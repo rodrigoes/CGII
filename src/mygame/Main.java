61fec26e7fb4f66cac86a330b106c6981632cb80
rodrigoes <jairrodrigo92@gmail.com>
2019-05-06 22:35:22 -0300

    Adicionado luz + modelo mesa de sinuca

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,11 +1,13 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 import com.jme3.texture.Texture;
 
@@ -23,18 +25,29 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        
+         
+    DirectionalLight sun = new DirectionalLight();
+    sun.setDirection((new Vector3f(-0.3f, -0.3f, -0.3f)).normalizeLocal());
+    sun.setColor(ColorRGBA.White);
+    
+    rootNode.addLight(sun);    
         flyCam.setMoveSpeed(60);
         cam.setLocation(new Vector3f(0,-1,0));
-        Box a = new Box(5, 0.2f, 5);
+        
+        Box a = new Box(10, 0.1f, 10);
         Geometry geom = new Geometry("Box", a);
-
+        
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         Texture monkeyTex = assetManager.loadTexture("Textures/piso.jpg"); 
         mat.setTexture("ColorMap", monkeyTex); 
         geom.setMaterial(mat);
     
+        Spatial myModel = assetManager.loadModel("/Textures/Mesa/PoolTable.j3o");
+         myModel.scale(0.09f);
+         rootNode.attachChild(myModel);
+    
         geom.setLocalTranslation(0, -4, -10.5f);
+        myModel.setLocalTranslation(0, -4, -10.5f);
 
 
         rootNode.attachChild(geom);
