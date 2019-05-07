ce4ae2daee7159439385679ce71560e6389fd568
rodrigoes <jairrodrigo92@gmail.com>
2019-05-06 21:31:29 -0300

    Adicionado piso/texture/ mudanca de posicao da camera inicial

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -3,9 +3,11 @@ package mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import com.jme3.texture.Texture;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -21,12 +23,19 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        
+        flyCam.setMoveSpeed(60);
+        cam.setLocation(new Vector3f(0,-1,0));
+        Box a = new Box(5, 0.2f, 5);
+        Geometry geom = new Geometry("Box", a);
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
+        Texture monkeyTex = assetManager.loadTexture("Textures/piso.jpg"); 
+        mat.setTexture("ColorMap", monkeyTex); 
         geom.setMaterial(mat);
+    
+        geom.setLocalTranslation(0, -4, -10.5f);
+
 
         rootNode.attachChild(geom);
     }
