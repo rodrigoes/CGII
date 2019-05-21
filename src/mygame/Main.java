ecc412c1d844827c0a17f8f20b87f3885f47e9b3
rodrigoes <jairrodrigo92@gmail.com>
2019-05-21 16:43:23 -0300

    Fisica bola,taco

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -4,6 +4,7 @@ import com.jme3.animation.LoopMode;
 import com.jme3.app.SimpleApplication;
 import com.jme3.audio.AudioData;
 import com.jme3.audio.AudioNode;
+import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.MouseButtonTrigger;
@@ -40,7 +41,7 @@ private Spatial PoolCue;
 private Spatial Table;
 private Spatial Ball;
 private Spatial VenetianBlind;
-
+private BulletAppState physicAppState;
 private AudioNode music;
 
 public static void main(String[] args) {
@@ -53,76 +54,96 @@ public static void main(String[] args) {
 
     @Override
     public void simpleInitApp() {
-     //  initLight();
+         //initLight();
        initKeys();
        initAudio();
+       initPhysics();
 
-SpotLight spot = new SpotLight();
-spot.setSpotRange(1000f);                           // distance
-spot.setSpotInnerAngle(15f * FastMath.DEG_TO_RAD); // inner light cone (central beam)
-spot.setSpotOuterAngle(35f * FastMath.DEG_TO_RAD); // outer light cone (edge of the light)
-spot.setColor(ColorRGBA.White.mult(15));         // light color
-spot.setPosition(new Vector3f(-1, 5, 5));               // shine from camera loc
-spot.setDirection(cam.getDirection());             // shine forward from camera loc
-rootNode.addLight(spot);
+        SpotLight spot = new SpotLight();
+        spot.setSpotRange(1000f);                           // distance
+        spot.setSpotInnerAngle(15f * FastMath.DEG_TO_RAD); // inner light cone (central beam)
+        spot.setSpotOuterAngle(35f * FastMath.DEG_TO_RAD); // outer light cone (edge of the light)
+        spot.setColor(ColorRGBA.White.mult(15));         // light color
+        spot.setPosition(new Vector3f(-1, 5, 5));               // shine from camera loc
+        spot.setDirection(cam.getDirection());             // shine forward from camera loc
+        rootNode.addLight(spot);
 
         flyCam.setMoveSpeed(60);
         cam.setLocation(new Vector3f(0, 1f, -3f));
 
         shootables = new Node("Shootables");
         shootables.attachChild(makeFloor(0, -4, -10.5f));
+        physicAppState.getPhysicsSpace().add(shootables);
         rootNode.attachChild(shootables);
-
+ 
         shootables = new Node("Shootables");
         shootables.attachChild(makeFloor(0, 6, -10.5f));
+        physicAppState.getPhysicsSpace().add(shootables);
         rootNode.attachChild(shootables);
-
+        
         wall = new Node("wall");
         wall.attachChild(makeWall(10, 1, -10.5f, 0.2f, 5, 15));
+        physicAppState.getPhysicsSpace().add(wall);
         rootNode.attachChild(wall);
 
         wall = new Node("wall");
-        wall.attachChild(makeWall(-10, 1, -10.5f, 0.2f, 5, 15));
+        wall.attachChild(makeWall(-10, 1, -10.5f, 0.2f, 5, 15));  
+        physicAppState.getPhysicsSpace().add(wall);
         rootNode.attachChild(wall);
-
+        
         wall = new Node("wall");
         wall.attachChild(makeWall(25, 1, 0, 0.2f, 5, 10));
         wall.rotate(0, 1.5708f, 0); //90 graus 1.5708 rad
+        physicAppState.getPhysicsSpace().add(wall);
         rootNode.attachChild(wall);
-
+        
         Table = assetManager.loadModel("/Models/Table/Table.j3o");
-        Table.scale(3);
+       // Table.setLocalScale(3);
+       Table.setLocalTranslation(0, -4, -10.5f);
+      // RigidBodyControl r = new RigidBodyControl();
+       //Table.addControl(r);
+        
+       // r.setPhysicsLocation(Table.getLocalTranslation());
+      //physicAppState.getPhysicsSpace().add(r);
         rootNode.attachChild(Table);
-
-      /*
-         Spatial Wardrobe = assetManager.loadModel("/Models/Others/Sideboard.obj");
-         Wardrobe.scale(20);
-         rootNode.attachChild(Wardrobe);*/
+      
+        
+        /*
+        Spatial Wardrobe = assetManager.loadModel("/Models/Others/Sideboard.obj");
+        Wardrobe.scale(20);
+        rootNode.attachChild(Wardrobe);*/
+        
         PoolCue = assetManager.loadModel("/Models/Others/Poolcue.j3o");
-       // Same Model
-    // IMPORTANT : You must navigate to the Geometry for this to work
-   // Geometry geom = ((Geometry) ((Node) PoolCue).getChild("MonkeyHeadGeom"));
-  //  geom.addControl(new RigidBodyControl(0));
-    // Works great (scaling of a MeshCollisionShape)
- //  geom.getControl(RigidBodyControl.class).getCollisionShape().setScale(new Vector3f(2, 2, 2));
-  //  bulletAppState.getPhysicsSpace().add(geom);
+  
         PoolCue.scale(3);
+        PoolCue.move(0, -3.9f, -10.5f);
+        RigidBodyControl r2 = new RigidBodyControl();
+        PoolCue.addControl(r2);
+        r2.setPhysicsLocation(PoolCue.getLocalTranslation());
+        physicAppState.getPhysicsSpace().add(r2);
         rootNode.attachChild(PoolCue);
-
+        
+        
         Ball = assetManager.loadModel("/Models/Others/ball.j3o");
-        Ball.scale(3);
+        Ball.setLocalScale(3);
+        Ball.move(0, -4, -10.5f);
+        RigidBodyControl r3 = new RigidBodyControl();
+        Ball.addControl(r3);
+        r3.setPhysicsLocation(Ball.getLocalTranslation());
+        physicAppState.getPhysicsSpace().add(r3);
         rootNode.attachChild(Ball);
         
         VenetianBlind = assetManager.loadModel("/Models/Others/VenetianBlind.j3o");
         VenetianBlind.scale(0.3f);
         VenetianBlind.rotate(0, 1.5708f, 0);
+        physicAppState.getPhysicsSpace().add(VenetianBlind);
         rootNode.attachChild(VenetianBlind);
-
-       // Wardrobe.setLocalTranslation(0, -4, -10.5f);
+        
+        // Wardrobe.setLocalTranslation(0, -4, -10.5f);
         VenetianBlind.setLocalTranslation(0, 0, -24.6f);
-        Table.setLocalTranslation(0, -4, -10.5f);
-        PoolCue.setLocalTranslation(0, -4f, -10.5f);
-        Ball.setLocalTranslation(0, -4, -10.5f);
+      //  Table.setLocalTranslation(0, -4, -10.5f);
+      //  PoolCue.setLocalTranslation(0, -3.9f, -10.5f);
+      //  Ball.setLocalTranslation(0, -4, -10.5f);
 
     }
 
@@ -147,10 +168,19 @@ rootNode.addLight(spot);
         geom.setMaterial(mat);
 
         geom.setLocalTranslation(x, y, z);
+        RigidBodyControl r = new RigidBodyControl(0);
+            
+        geom.addControl(r);
 
+       
+        r.setPhysicsLocation(geom.getLocalTranslation());
+        
+        rootNode.attachChild(geom);
+        physicAppState.getPhysicsSpace().add(r);
         return geom;
 
     }
+    
 //(10, 1, -10.5f)
 
     protected Geometry makeWall(int xpos, int ypos, float zpos, float xtam, int ytam, int ztam) {
@@ -168,22 +198,18 @@ rootNode.addLight(spot);
         return geom;
 
     }
+    
       private void initLight() {
           
-        SpotLight spot = new SpotLight();
-        spot.setSpotRange(1000f);                               // distance
-        spot.setSpotInnerAngle(15f * FastMath.DEG_TO_RAD);      // inner light cone (central beam)
-        spot.setSpotOuterAngle(35f * FastMath.DEG_TO_RAD);      // outer light cone (edge of the light)
-        spot.setColor(ColorRGBA.White.mult(15));                // light color
-        spot.setPosition(new Vector3f(-1, 5, 5));               // shine from camera loc
-        spot.setDirection(cam.getDirection());                  // shine forward from camera loc
-        rootNode.addLight(spot);
-
+        AmbientLight al = new AmbientLight();
+        al.setColor(ColorRGBA.White.mult(1.3f));
+        rootNode.addLight(al);
+        
         DirectionalLight sun = new DirectionalLight();
         sun.setColor(ColorRGBA.White);
-        sun.setDirection(cam.getDirection());
+        sun.setDirection(cam.getDirection().setY(10f));
         rootNode.addLight(sun);
-       
+        
         final int SHADOWMAP_SIZE = 1024;
         DirectionalLightShadowRenderer dlsr = new DirectionalLightShadowRenderer(assetManager, SHADOWMAP_SIZE, 3);
         dlsr.setLight(sun);
@@ -192,7 +218,6 @@ rootNode.addLight(spot);
         DirectionalLightShadowFilter dlsf = new DirectionalLightShadowFilter(assetManager, SHADOWMAP_SIZE, 3);
         dlsf.setLight(sun);
         dlsf.setEnabled(true);
-        
         FilterPostProcessor fpp = new FilterPostProcessor(assetManager);
         fpp.addFilter(dlsf);
         viewPort.addProcessor(fpp);
@@ -228,24 +253,34 @@ rootNode.addLight(spot);
         
         private void initKeys(){
             inputManager.addMapping("play", new MouseButtonTrigger(mouseInput.BUTTON_LEFT));
+            inputManager.addMapping("play2", new MouseButtonTrigger(mouseInput.BUTTON_RIGHT));
             inputManager.addListener(actionListener, "play");
+            inputManager.addListener(actionListener, "play2");
         }
         
         private ActionListener actionListener = new ActionListener() { 
+        @Override
         public void onAction(String name, boolean keyPressed, float tpf) {
             
             if (name.equals("play") && !keyPressed) {
-                hitPoolCue();
+                hitPoolCue(0,0,2);
+            }
+            if (name.equals("play2") && !keyPressed) {
+                hitPoolCue(0,0,-2);
             }
            
         }
     };
         
-        private void hitPoolCue(){
-            
-            PoolCue.move(0, 0, 2);
-            
-        
+        private void hitPoolCue(int x,int y,int z){
+          
+            PoolCue.move(x, y, z);
             
         }
+        
+        private void initPhysics() {
+        physicAppState = new BulletAppState();
+      //  physicAppState.setDebugEnabled(true);
+        stateManager.attach(physicAppState);
+    }
 }
